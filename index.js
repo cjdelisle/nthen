6ca4a9a8bcd32202759693f7455b4afc0492e5f3
Caleb James DeLisle <cjd@cjdns.fr>
2017-03-31 22:50:51 +0200

    implement flowtypes, add bower and jshint

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+/*@flow*/
 /*
  * Caleb James DeLisle
  * Sat Mar 23 01:42:29 EDT 2013
- * Public Domain
+ * Public Domain or MIT License
  */
 ;(function() {
-var nThen = function(next) {
+/*::
+type WaitFor_t = (...Array<any>)=>(...Array<any>)=>void;
+type NthenRet_t = { nThen: Nthen_t, orTimeout:((WaitFor_t)=>void, number)=>NthenRet_t };
+type Nthen_t = ((WaitFor_t)=>void)=>NthenRet_t;
+*/
+var nThen /*:Nthen_t*/ = function(next) {
     var funcs = [];
     var calls = 0;
     var waitFor = function(func) {
@@ -32,7 +38,7 @@ var nThen = function(next) {
             if (!milliseconds) { throw Error("Must specify milliseconds to orTimeout()"); }
             var cto;
             var timeout = setTimeout(function() {
-                while (funcs.shift() !== cto) ;
+                while (funcs.shift() !== cto) { }
                 func(waitFor);
                 calls = (calls || 1) - 1;
                 while (!calls && funcs.length) { funcs.shift()(waitFor); }
@@ -44,14 +50,15 @@ var nThen = function(next) {
     return ret.nThen(next);
 };
 
-if (typeof(define) == 'function') {
-    // AMD (require.js etc)
-    define([], function() { return nThen; });
-} else if (typeof(window) !== 'undefined') {
-    // Browser global var nThen
-    window.nThen = nThen;
-}
-if (typeof(module) !== 'undefined' && module.exports) {
+if (typeof(window) === 'object') {
+    if (typeof(window.define) === 'function') {
+        // AMD (require.js etc)
+        window.define([], function() { return nThen; });
+    } else {
+        // Browser global var nThen
+        window.nThen = nThen;
+    }
+} else if (typeof(module) !== 'undefined' && module.exports) {
     // Node.js
     module.exports = nThen;
 }
